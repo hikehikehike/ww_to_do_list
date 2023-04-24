66d8287e7c060a49111830f20274cc22934db19a
Timur <timur.hike@gmail.com>
2023-04-24 13:17:23 +0300

    Add user in task model

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="task")
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="task")
     header = models.CharField(max_length=255)
     content = models.TextField()
     datetime = models.DateTimeField(auto_now=True)
