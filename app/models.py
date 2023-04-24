d59fbfb74fa413b7c3bc06c2c15e4871687e9eb0
Timur <timur.hike@gmail.com>
2023-04-24 12:41:57 +0300

    Add task models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="task")
+    header = models.CharField(max_length=255)
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done_or_not = models.BooleanField()
