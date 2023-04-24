9eeb2c1f9cc24f0e70ebf37bb4ee6ea15c084f56
Timur <timur.hike@gmail.com>
2023-04-24 12:35:20 +0300

    Add user app in setting

--- ww_to_do_list/settings.py
+++ ww_to_do_list/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+AUTH_USER_MODEL = "user.User"
 
 LANGUAGE_CODE = "en-us"
 
