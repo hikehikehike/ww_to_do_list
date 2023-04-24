4060bb8f87095ee0283b1777b1a7d99dc49a19d1
Timur <timur.hike@gmail.com>
2023-04-24 13:35:00 +0300

    Add views and complete_undo

--- app/views.py
+++ app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from app.forms import TaskForm
+from app.models import Task
+
+
+class TaskListViews(generic.ListView):
+    model = Task
+    fields = "__all__"
+
+
+class TaskDetailViews(generic.DetailView):
+    model = Task
+    fields = "__all__"
+
+
+class TaskCreationViews(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todolist:task-list")
+
+
+class TaskUpdateViews(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todolist:task-list")
+
+
+class TaskDeleteViews(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todolist:task-list")
+
+
+def complete_undo(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.done_or_not = not task.done_or_not
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todolist:task-list"))
