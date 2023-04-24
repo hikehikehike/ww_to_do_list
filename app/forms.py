6a2e54095cfe8dc9c496e7117037f33ce53dc0b4
Timur <timur.hike@gmail.com>
2023-04-24 15:53:08 +0300

    Add placeholder to deadline

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import DateTimeInput
 
 from app.models import Task
 
@@ -6,5 +7,14 @@ from app.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = [
+            "user",
+            "header",
+            "content",
+            "deadline",
+            "done_or_not",
+        ]
         exclude = ("user",)
+        widgets = {
+            "deadline": DateTimeInput(attrs={"placeholder": "Enter deadline like: 2022-12-25"}),
+        }
